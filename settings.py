d40c9345c0f9afe188c82ae481414d1b42def5e5
Kevin Bay <kevin.bay@outlook.com>
2019-04-07 14:57:12 +0200

    Added trigger to control timed option

--- settings.py
+++ settings.py
@@ -6,4 +6,8 @@ OVERRIDE_SUBMISSIONS = False
 OVERRIDE_LIST = ['afd9jw']
 
 WRITE_REPLIES = True
+
+RUN_TIMED = False
 WAITTIME = 480
+
+NOTIFY_URL = 'https://notify.run/a0Kgd3y6dzldyVDk'
--- srgrafobot.py
+++ srgrafobot.py
@@ -61,9 +61,9 @@ def check_for_submissions():
         logger.info("---------------------")
         logger.info("Starting the process now for ID %s", entry)
         make_entry(entry)
-    wait(WAITTIME)
 
-    'If not, sleep and repeat in 5 minutes'
+    if RUN_TIMED:
+        wait(WAITTIME)
 
 
 def check_for_current_submissions():
@@ -254,7 +254,7 @@ def create_full_post_content(submission):
 
             if entry[0] == 'srgrafo_edit_bot':
                 entry[1] = '[HA! YOU ALMOST GOT ME TO CREATE A BLACK HOLE, FELLOW HUMAN! BUT AS GOOD HUMAN, I SAW THROUGH YOUR EFFORTS, HAHA]'
-                
+
             body += "|" + str(i) + "|/u/" + entry[0] + "|" + entry[1] + "|" + entry[3] + "|[Link](" + entry[
                 4] + ")|" + "\n"
 
