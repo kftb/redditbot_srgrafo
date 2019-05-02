b5d4016df45d7c05157d5fc5513873e608df258e
Kevin Bay <kevin.bay@outlook.com>
2019-05-03 01:38:27 +0200

    Fixed error in title message

--- srgrafobot.py
+++ srgrafobot.py
@@ -255,10 +255,9 @@ def create_full_post_content(submission):
             if entry[0] == 'srgrafo_edit_bot':
                 entry[1] = '[HA! YOU ALMOST GOT ME TO CREATE A BLACK HOLE, FELLOW HUMAN! BUT AS GOOD HUMAN, I SAW THROUGH YOUR EFFORTS, HAHA]'
 
-            body += "|" + str(i) + "|/u/" + entry[0] + "|" + entry[1] + "|" + entry[3] + "|[Link](" + entry[
-                4] + ")|" + "\n"
+            body += "|" + str(i) + "|/u/" + entry[0] + "|" + entry[1] + "|" + entry[3] + "|[Link](" + entry[4] + ")|" + "\n"
 
-        body += "\n \n" + "I am a little fan-made bot who loves /u/SrGrafo but is a little lazy with hunting for EDITs. If you want to support our great creator, check out his [Patreon](Patreon.com/SrGrafo)"
+        body += "\n \n" + "I am a little fan-made bot who loves /u/SrGrafo but is a little lazy with hunting for EDITs. If you want to support our great creator, check out his [Patreon]("Patreon.com/SrGrafo")"
 
         return body
 
