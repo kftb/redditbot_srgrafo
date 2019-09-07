92da331f0cfbb8a9fae0652a29bf36142216f7f3
Kevin Bay <kevin.bay@outlook.com>
2019-09-07 20:15:54 +0200

    Outsourced message to settings, changed message

--- settings.py
+++ settings.py
@@ -11,3 +11,5 @@ RUN_TIMED = False
 WAITTIME = 480
 
 NOTIFY_URL = 'https://notify.run/a0Kgd3y6dzldyVDk'
+
+MESSAGE_END = "I am a little fan-made bot who loves /u/SrGrafo but is a little lazy with hunting for EDITs. If you want to support our great creator, check out his [Patreon](https://Patreon.com/SrGrafo). If you like this me, feel free to reply with 'Good bot'"
