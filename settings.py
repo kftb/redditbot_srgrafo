627489c2e1b5bb76bc7bd16cb6c8861a96b70923
Kevin Bay <kevin.bay@outlook.com>
2019-09-07 20:32:58 +0200

    Changed message at the end

--- settings.py
+++ settings.py
@@ -12,4 +12,4 @@ WAITTIME = 480
 
 NOTIFY_URL = 'https://notify.run/a0Kgd3y6dzldyVDk'
 
-MESSAGE_END = "I am a little fan-made bot who loves /u/SrGrafo but is a little lazy with hunting for EDITs. If you want to support our great creator, check out his [Patreon](https://Patreon.com/SrGrafo). If you like this me, feel free to reply with 'Good bot'"
+MESSAGE_END = "I am a little fan-made bot who loves /u/SrGrafo but is a little lazy with hunting for EDITs. If you want to support our great creator, check out his [Patreon](https://Patreon.com/SrGrafo). If you like enjoy my hard robot work, a 'good bot' reply would make me very happy."
