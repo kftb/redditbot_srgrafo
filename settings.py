70587f8e0575ab3ab4fd5a19b7cb5986b74fcce6
Kevin Bay <kevin.bay@outlook.com>
2019-09-12 23:24:49 +0200

    Adjusted final message to include subreddit rules

--- settings.py
+++ settings.py
@@ -12,4 +12,4 @@ WAITTIME = 480
 
 NOTIFY_URL = 'https://notify.run/a0Kgd3y6dzldyVDk'
 
-MESSAGE_END = "I am a little fan-made bot who loves /u/SrGrafo but is a little lazy with hunting for EDITs. If you want to support our great creator, check out his [Patreon](https://Patreon.com/SrGrafo). If you enjoy my hard robot work, a 'good bot' reply would make me very happy."
+MESSAGE_END = "I am a little fan-made bot who loves /u/SrGrafo but is a little lazy with hunting for EDITs. If you want to support our great creator, check out his [Patreon](https://Patreon.com/SrGrafo). If you enjoy my hard robot work (and it's allowed by the subreddit rules), a 'good bot' reply would make me very happy."
